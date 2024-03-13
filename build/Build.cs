b31dd4eada2b259ac05f6016cd4bde9246769e38
fennekin23 <bogdan.sachkovskyi@gmail.com>
2024-03-13 22:45:59 +0200

    ci: use `MinVer` (#7)

--- build/Build.cs
+++ build/Build.cs
@@ -3,9 +3,12 @@ using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MinVer;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [ShutdownDotNetAfterServerBuild]
@@ -29,17 +32,17 @@ class Build : NukeBuild
 
     public static int Main () => Execute<Build>(x => x.Compile);
 
+    [MinVer]
+    readonly MinVer MinVer;
+    
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution(GenerateProjects = true, SuppressBuildProjectCheck = true)]
     readonly Solution Solution;
 
-    AbsolutePath SourceDirectory => RootDirectory / "src";
+    readonly AbsolutePath SourceDirectory = RootDirectory / "src";
 
-    [GitVersion]
-    readonly GitVersion GitVersion;
-    
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
@@ -50,8 +53,7 @@ class Build : NukeBuild
     Target Restore => _ => _
         .Executes(() =>
         {
-            DotNetRestore(s => 
-                s.SetProjectFile(Solution));
+            DotNetRestore(s => s.SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
@@ -59,15 +61,15 @@ class Build : NukeBuild
         .Executes(() =>
         {
             ReportSummary(s =>
-                s.AddPairWhenValueNotNull("Version", GitVersion.SemVer));
+                s.AddPairWhenValueNotNull("Version", MinVer.MinVerVersion));
             
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
                 .SetContinuousIntegrationBuild(IsServerBuild)
-                .SetAssemblyVersion(GitVersion.AssemblySemVer)
-                .SetFileVersion(GitVersion.AssemblySemFileVer)
-                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .SetAssemblyVersion(MinVer.AssemblyVersion)
+                .SetFileVersion(MinVer.FileVersion)
+                .SetInformationalVersion(MinVer.MinVerVersion)
                 .EnableNoLogo()
                 .EnableNoRestore());
         });
