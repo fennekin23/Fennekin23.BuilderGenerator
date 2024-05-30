19b2e42a84b592b0ad46ec63859451334ac65ae1
fennekin23 <bogdan.sachkovskyi@gmail.com>
2024-05-30 22:05:18 +0300

    Add support for `class` builder (#27)

--- tests/Fennekin23.BuilderGenerator.Tests/Samples/SampleInternalPositionalRecord.cs
+++ tests/Fennekin23.BuilderGenerator.Tests/Samples/SampleInternalPositionalRecord.cs
@@ -4,6 +4,6 @@ namespace Fennekin23.BuilderGenerator.Tests.Samples;
 internal record SampleInternalPositionalRecord(string StringProperty, int IntProperty)
 {
     public bool BooleanProperty { get; init; }
-             
+
     public long LongProperty { get; set; }
 }
---
+++ tests/Fennekin23.BuilderGenerator.Tests/Samples/SamplePublicConstructorClass.cs
@@ -0,0 +1,19 @@
+﻿namespace Fennekin23.BuilderGenerator.Tests;
+
+[BuilderGenerator]
+public record SamplePublicConstructorClass
+{
+    public string StringProperty { get; }
+    public int IntProperty { get; }
+
+    public SamplePublicConstructorClass(string stringProperty)
+    {
+        StringProperty = stringProperty;
+    }
+
+    public SamplePublicConstructorClass(string stringProperty, int intProperty)
+    {
+        StringProperty = stringProperty;
+        IntProperty = intProperty;
+    }
+}
